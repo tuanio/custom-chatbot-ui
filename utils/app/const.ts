494e9234a29750852ff918e87301730728e84e4a
Tuan <nvatuan3@gmail.com>
2023-05-13 12:08:33 +0700

    temp

---
+++ utils/app/const.ts
@@ -0,0 +1,24 @@
+export const DEFAULT_SYSTEM_PROMPT =
+  process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
+  "You are DemandGPT, a large language model trained by JQ. Follow the user's instructions carefully. Respond using markdown.";
+
+  // "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+
+export const OPENAI_API_HOST =
+  // process.env.OPENAI_API_HOST || 'https://api.openai.com';
+  process.env.OPENAI_API_HOST || 'https://12d3-34-31-56-228.ngrok-free.app';
+
+export const DEFAULT_TEMPERATURE = 
+  parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.1");
+
+export const OPENAI_API_TYPE =
+  process.env.OPENAI_API_TYPE || 'openai';
+
+export const OPENAI_API_VERSION =
+  process.env.OPENAI_API_VERSION || '2023-03-15-preview';
+
+export const OPENAI_ORGANIZATION =
+  process.env.OPENAI_ORGANIZATION || '';
+
+export const AZURE_DEPLOYMENT_ID =
+  process.env.AZURE_DEPLOYMENT_ID || '';
